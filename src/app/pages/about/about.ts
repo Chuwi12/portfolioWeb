440368b9f0dc3adb3a46d0b914516d0470e4b76c
chuwi <srpc9682@protonmail.com>
2026-05-30 22:40:46 +0200

    fix: medium security vulnerabilities (XSS, CSP, Host Header)

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -7,11 +7,4 @@ import { Component } from '@angular/core';
   styleUrl: './about.css',
 })
 export class About {
-  
-  protected readonly biography: string = 
-    'Soy Sergio Rodríguez, un desarrollador enfocado en la realización de soluciones backend eficientes y escalables. ' +
-    'Actualmente, me encuentro <strong>finalizando mis estudios en el Grado Superior de Desarrollo de Aplicaciones Multiplataforma (DAM)</strong>. ' +
-    'Como apasionado de la tecnología, siempre estoy buscando aprender <strong>nuevas herramientas</strong>; ' + 
-    'estoy especializado en desarrollos con <strong>Java</strong>, aunque también tengo experiencia desarrollando con <strong>Python</strong>. ' +
-    'En estos momentos, estoy profundizando en el lenguaje Rust y en patrones de diseño, con el objetivo de diseñar arquitecturas de software robustas y resolver problemas complejos.'; 
 }
