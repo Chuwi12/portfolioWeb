d5eee3244452ef621358db687d5cca137046eea9
chuwi <srpc9682@protonmail.com>
2026-05-30 15:21:01 +0200

    feat: add vercel analytics and speed insights

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './core/footer/footer';
 import { Header } from './core/header/header';
+import { inject as injectAnalytics } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Footer, Header],
-  templateUrl: './app.html', 
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      injectAnalytics();
+      injectSpeedInsights();
+    }
+  }
 }
